d87150cf95b2314f75f5584501d082963c6cbf34
Shikhar Chandra <47221817+SHIKHAR-CHANDRA@users.noreply.github.com>
2021-10-16 21:55:18 +0530

    rearranged files

--- SortData.m
+++
@@ -1,7 +0,0 @@
-load data.csv
-eeg_signals = reshape((data)',178*23,500)'; %reshaping from [11500 X 179] to [500 X 4094]
-time = (1:4094);
-eeg_sample = eeg_signals(2:2,:);
-plot(time,eeg_sample),title("Sample EEG signal"),
-xlabel("Time"),
-ylabel("Amplitude (uV)");
--- seizure_classes.m
+++
@@ -1,3 +0,0 @@
-load data1.csv
-seizure_info = reshape(data1',23,500)';
-seizure_info = seizure_info ~= 1; % storing seizure seconds in a new variable
